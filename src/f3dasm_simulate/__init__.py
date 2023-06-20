71662aa821064c211d688c661a8c5c2a16e02b09
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-06-20 17:44:50 +0200

    added entry level __init__.py

---
+++ src/f3dasm_simulate/__init__.py
@@ -0,0 +1,31 @@
+"""
+f3dasm_simulate - Simulation extension of f3dasm
+the Bessa Group's conventions
+
+This package contains a template for Python packages following
+the Bessa Group's conventions.
+
+Usage:
+  import f3dasm_simulate
+
+Author: Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)
+"""
+
+#                                                                       Modules
+# =============================================================================
+
+# Standard
+
+# Third-party
+
+from . import abaqus
+from .example_module import add_one
+
+#                                                        Authorship and Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
