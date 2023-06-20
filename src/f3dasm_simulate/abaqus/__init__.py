52b50ca0f6353a242c1c844c007dccbc2fceb034
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-06-20 17:44:38 +0200

    changed __init__.py for example_module

--- src/f3dasm_simulate/abaqus/__init__.py
+++ src/f3dasm_simulate/abaqus/__init__.py
@@ -21,7 +21,7 @@ Author: Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)
 from . import loading, material, microstructure, simulator, simulator_info
 # Local
 from .cddm import VonMisesPlasticElasticPathLoads
-from .example_module import add_one
+from ..example_module import add_one
 
 #                                                        Authorship and Credits
 # =============================================================================
